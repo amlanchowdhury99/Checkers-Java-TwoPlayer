cc6540b408f59dec9bb8f6689e3a9c568086b801
Amlan Chowdhury <amlanchowdhury99@gmail.com>
2017-05-09 10:58:08 +0600

    Java Project
    Client Server Java Project

---
+++ Draught-TwoPlayer/src/ClientPackage/ListenForMouse.java
@@ -0,0 +1,38 @@
+package ClientPackage;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+/*
+ * Listener for mouse click, used when game is not running
+ */
+public class ListenForMouse implements MouseListener {
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
